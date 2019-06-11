fd89cbfdb831ca6302d84c388fc3454c8a16a14a
ivanstepanov <ivanstepanoff@yahoo.com>
2019-06-12 01:11:56 +0200

    Recover create-react-app dependencies

---
+++ react-weather/README.md
@@ -0,0 +1,35 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# weather-react
+
+## Available Scripts
+In the project directory, you can run:
+
+### `npm instal`
+
+To install all needed dependences
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+geo coords of Kaliningrad:<br>
+54° 43' 12" (54° 43' 27) north latitude<br>
+20° 31' 18" (20° 31' 44) east long<br>
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br>
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br>
+`The project was built assuming it is hosted at the server root.
+You can control this with the homepage field in your package.json.
+For example, add this to build it for GitHub Pages:
+  "homepage" : "http://myname.github.io/myapp",
+The build folder is ready to be deployed.
+You may serve it with a static server:
+  serve -s build
+Find out more about deployment here:
+  [https://bit.ly/CRA-deploy](https://bit.ly/CRA-deploy) `
+
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
---
+++ react-weather/src/App.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = "95b86822741aadf615e1da0ac5ded8b3",
+    APP_ID = "ab684c34";
+// API Base URL: http://api.weatherunlocked.com/ or https://api.weatherunlocked.com/
+// api/{LocalWeatherType}/{Location}?app_id={APP_ID}&app_key={APP_KEY}
+// {LocalWeatherType} can be "current" or "forecast" Example:
+// http://api.weatherunlocked.com/api/current/51.50,-0.12?app_id={APP_ID}&app_key={APP_KEY}
+// Valid decimal geolocation coordinates with the following format: "latitude,longitude", with no more than 3 decimal places  OR postcodes:
+//  (@ = Letter, # = Number)
+// United Kingdom  'uk'  'uk.postcode'
+// United States 'us'  'us.#####'
+// Example: Current Weather and UK postcode
+// http://api.weatherunlocked.com/api/current/uk.G3 8ND?app_id=APP_ID&app_key=APP_KEY
+// Example: Forecast Weather and US zipcode
+// http://api.weatherunlocked.com/api/forecast/us.33109?app_id=APP_ID&app_key=APP_KEY
+//  and local language :
+// Example: Forecast Weather for NY with Italian descriptions
+// http://api.weatherunlocked.com/api/forecast/40.7,-74?lang=it&app_id=APP_ID&app_key=APP_KEY
+
+class App extends React.Component {
+    // depricated method in React >=v.16 !!!!! "constructor ditched altogether"
+    // constructor(props) {
+    //   super(props);
+    //   this.state = {
+    //   }
+    // }
+    state = {
+        cloudtotal: undefined,    //index
+        feelslike: undefined,    //C
+        humidity: undefined,    //%
+        seaLevelPressure:  undefined,   //millibars
+        temperature: undefined,    //C
+        winddir_compass: undefined,    //string
+        windSpeed: undefined,    //ms
+        description: undefined ,   //string
+        error: undefined
+    }
+
+  getWeather = async (e) => {
+
+    e.preventDefault();
+    // const country = e.target.elements.country.value;
+    // const city = e.target.elements.city.value;
+
+    const latitude = e.target.elements.latitude.value;
+    const longtitude = e.target.elements.longtitude.value;
+
+    // here we need to convert the latitude and the longitude to the town/ country names  (Russian post indexes are not available)
+    const api_call = await fetch(`http://api.weatherunlocked.com/api/current/${latitude},${longtitude}?lang=it&app_id=${APP_ID}&app_key=${API_KEY}`);
+    const data = await api_call.json();
+    if(latitude && longtitude) {
+        this.setState({
+        cloudtotal: data.cloudtotal_pct,    //index
+        feelslike: data.feelslike_c,    //C
+        humidity: data.humid_pct,    //%
+        seaLevelPressure:  data.slp_mb,   //millibars
+        temperature: data.temp_c,    //C
+        winddir_compass: data.winddir_compass,    //string
+        windSpeed: data.windspd_ms,    //ms
+        description: data.wx_desc ,   //string
+        error: ""
+        });
+        } else {
+        this.setState({
+        cloudtotal: undefined,    //index
+        feelslike: undefined,    //C
+        humidity: undefined,    //%
+        seaLevelPressure:  undefined,   //millibars
+        temperature: undefined,    //C
+        winddir_compass: undefined,    //string
+        windSpeed: undefined,    //ms
+        description: undefined ,   //string
+        error: "Enter the coords, please!"
+        });
+    }
+  }
+
+  render() {
+    return (
+        <div>
+            <div className="wrapper">
+                <div className="main">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xs-5 title-container">
+                                <Titles />
+                            </div>
+                            <div className="col-xs-7 form-container">
+                                <Form getWeather={this.getWeather}/>
+                                <Weather cloudtotal={this.state.cloudtotal}    //index
+                                    feelslike = {this.state.feelslike}    //C
+                                    humidity = {this.state.humidity}    //%
+                                    seaLevelPressure =  {this.state.seaLevelPressure}   //millibars
+                                    temperature = {this.state.temperature}    //C
+                                    winddir_compass = {this.state.winddir_compass}    //string
+                                    windSpeed = {this.state.windSpeed}    //ms
+                                    description = {this.state.description }   //string
+                                    error = {this.state.error } />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+      </div>);
+  }
+};
+
+export default App;
