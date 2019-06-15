6f9836d30d6239a54e2887066950b1855038bdea
ivanstepanov <ivanstepanoff@yahoo.com>
2019-06-15 07:28:02 +0200

    Create a component for constants

--- WeatherFinder/src/App.jsx
+++ WeatherFinder/src/App.jsx
@@ -1,29 +1,10 @@
-// 1.Убрать инициализацию stste пустыми значениями 2.Убрать константы в компонент 3.составить регулярку для подставления разделителя координат - , страны и индекса - . 4.Очищать state при нажатии на батон ()
+// 1.Убрать инициализацию stste пустыми значениями  3.Очищать state при нажатии на батон ()
 
 import React from 'react';
 import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
-
-const API_KEY = "95b86822741aadf615e1da0ac5ded8b3",
-    APP_ID = "ab684c34",
-    BASE_PATH = 'http://api.weatherunlocked.com/api',
-    LOCAL_WEATHER_TYPE = 'current';
-// API Base URL: http://api.weatherunlocked.com/ or https://api.weatherunlocked.com/
-// api/{LocalWeatherType}/{Location}?app_id={APP_ID}&app_key={APP_KEY}
-// {LocalWeatherType} can be "current" or "forecast" Example:
-// http://api.weatherunlocked.com/api/current/51.50,-0.12?app_id={APP_ID}&app_key={APP_KEY}
-// Valid decimal geolocation coordinates with the following format: "latitude,longitude", with no more than 3 decimal places  OR postcodes:
-//  (@ = Letter, # = Number)
-// United Kingdom  'uk'  'uk.postcode'
-// United States 'us'  'us.#####'
-// Example: Current Weather and UK postcode
-// http://api.weatherunlocked.com/api/current/uk.G3 8ND?app_id=APP_ID&app_key=APP_KEY
-// Example: Forecast Weather and US zipcode
-// http://api.weatherunlocked.com/api/forecast/us.33109?app_id=APP_ID&app_key=APP_KEY
-//  and local language :
-// Example: Forecast Weather for NY with Italian descriptions
-// http://api.weatherunlocked.com/api/forecast/40.7,-74?lang=it&app_id=APP_ID&app_key=APP_KEY
+import {API_KEY, APP_ID, BASE_PATH, LOCAL_WEATHER_TYPE} from './constants';
 
 class App extends React.Component {
 
