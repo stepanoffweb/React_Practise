7895f01c622095dc7af7453a6b942398c74d66f1
ivanstepanov <ivanstepanoff@yahoo.com>
2020-04-20 16:35:01 +0300

    kamasutra Messages page styling

--- kamasutra/social-network/src/components/Dialogs/Dialogs.jsx
+++ kamasutra/social-network/src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,21 @@ import s from './Dialogs.module.css'
 const Dialogs = () => {
     return (
         <div>
-            Dialogs
+            <div className={s.dialogs}>
+                <div className={s.dialogItems}>
+                    <div className={s.dialogItem + ' ' + s.active}>Name-1</div>
+                    <div className={s.dialogItem}>Name-2</div>
+                    <div className={s.dialogItem}>Name-3</div>
+                    <div className={s.dialogItem}>Name-4</div>
+                    <div className={s.dialogItem}>Name-5</div>
+                    <div className={s.dialogItem}>Name-6</div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>What's up?</div>
+                    <div className={s.message}>How is Your webDev?</div>
+                    <div className={s.message}>Yo!!!</div>
+                </div>
+            </div>
         </div>
     )
 }
