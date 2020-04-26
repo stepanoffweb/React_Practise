2e60aa699fb8b9aec4c6a22d0f1cc2bfdc6c991a
ivanstepanov <ivanstepanoff@yahoo.com>
2020-04-26 09:39:09 +0300

    create kamasutra-1 branch

--- kamasutra/social-network/src/components/Dialogs/Message/Message.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import s from '../Dialogs.module.css'
-
-const Message = ({message, id}) => {
-    return <div id={id} className={s.message}>{message}</div>
-}
-
-export default Message
