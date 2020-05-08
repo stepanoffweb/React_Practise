8addc376d384a0d76190fa073894bd45c355e519
ivanstepanov <ivanstepanoff@yahoo.com>
2020-05-08 17:27:09 +0300

    start

---
+++ pokemon/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from 'axios'
+
+function App() {
+
+  return (
+    <div className="App">
+      // <h1>{name.results[2].name}</h1>
+    </div>
+  );
+}
+
+export default App;
