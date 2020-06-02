680d5daaf5c87e61bb76d2c6eaffbc9acc96eeb0
ivanstepanov <ivanstepanoff@yahoo.com>
2020-06-02 20:57:38 +0300

    kamasutra-93: Paginator.jsx

--- kamasutra/social-network/src/components/Users/Users.jsx
+++ kamasutra/social-network/src/components/Users/Users.jsx
@@ -1,35 +1,25 @@
 import React from 'react'
+
 import s from './Users.module.css'
 import User from './User/User'
+import {Paginator} from '../common/Paginator/Paginator'
 
-const Users = (props) => {
+const Users = ({followingInProgress, handleFollow, handleUnfollow, totalUsersCount, pageSize, handleClickPage, currentPage, users, ...props }) => {
 
-  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
-  let pages = []
-    let i = 1
-    while(i <= pagesCount) {
-      pages.push(i)
-      i++
-    }
-    // if (this.props.isFetching) return <Preloader />
-    // let followingInProgress = this.props.followingInProgress
 
-  return <>
-    <div className={s.usersWrapper}>
-      {pages.map(p => <span key={p} className={`${s.pagination} ${props.currentPage === p ? s.selected : ''}`}
-        onClick={() => props.handleClickPage(p)} >{p}</span>)}
+  return <div className={s.usersWrapper}>
+      <Paginator totalUsersCount={totalUsersCount} pageSize={pageSize} handleClickPage={handleClickPage} currentPage={currentPage} />
 
-      {props.users.map(({id, name, status, country, city, followed, photos:{small}}) =>
-        <User key={`${id}-${status}`}
-              userId={id} name={name} status={status} country={country}
-              city={city} followed={followed} pic={small}
-              followingInProgress={props.followingInProgress}
-              handleFollow={() => props.handleFollow(id)}
-              handleUnfollow={() => props.handleUnfollow(id)} /> )}
+        {users.map(({id, name, status, country, city, followed, photos:{small}}) =>
+          <User key={`${id}-${status}`}
+                userId={id} name={name} status={status} country={country}
+                city={city} followed={followed} pic={small}
+                followingInProgress={followingInProgress}
+                handleFollow={() => handleFollow(id)}
+                handleUnfollow={() => handleUnfollow(id)} /> )}
 
-      <button className={s.show}>Show More</button>
     </div>
-  </>
+
 }
 
 export default Users;
