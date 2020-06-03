90e4eed3acde42a271a52576edfc76d923f578d2
ivanstepanov <ivanstepanoff@yahoo.com>
2020-06-03 08:28:44 +0300

    kamasutra: created branch soc-net & deleted content kamasutra/social-network/

--- kamasutra/social-network/src/utils/object-helpers.js
+++
@@ -1,5 +0,0 @@
-export const updateObjectInArray = (items, itemId, objPropName, newObjProps) => (
-  // typeof objPropName === stryng!!!
-
-  items.map(item => (item[objPropName] === itemId) ? {...item, ...newObjProps} : item)
-)
