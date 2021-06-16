43873e17f2f01657b94069f8d952efee7c21b13c
ivanstepanov <ivanstepanoff@yahoo.com>
2021-06-16 17:49:06 +0200

    todo_back is deleted

--- todo_back/src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export const Loader = () => {
-  return (
-    <div className="spinner-border text-primary">
-      <span className="visually-hidden">Loading...</span>
-    </div>
-  )
-}
--- todo_back/src/context/types.js
+++
@@ -1,6 +0,0 @@
-export const SHOW_ALERT = "SHOW_ALERT"
-export const HIDE_ALERT = "HIDE_ALERT"
-export const SHOW_LOADER = "SHOW_LOADER"
-export const FETCH_BASE = "FETCH_BASE"
-export const ADD_NOTE = "ADD_NOTE"
-export const REMOVE_NOTE = "REMOVE_NOTE"
